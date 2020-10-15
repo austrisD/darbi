0e6932d1e00004ef28374d0a61f2c50ef90f02f9
austrisD <austris.daugulis@gmail.com>
2020-10-15 15:13:13 +0300

    add fonts

---
+++ moto_page/src/data/data.js
@@ -0,0 +1,62 @@
+class motorConstruct {
+  constructor(
+    Brand,
+    model,
+    year,
+    cc,
+    hp,
+    nm,
+    weight,
+    setHeight,
+    brake,
+    color,
+    desc
+  ) {
+    this.Brand = Brand;
+    this.model = model;
+    this.year = year;
+    this.cc = cc;
+    this.hp = hp;
+    this.nm = nm;
+    this.weight = weight;
+    this.setHeight = setHeight;
+    this.brake = brake;
+    this.color = color;
+    this.desc = desc;
+  }
+}
+
+export const motorSpec = [
+  new motorConstruct(
+    "Ducati",
+    "Monster",
+    2019,
+    803,
+    73,
+    67,
+    175,
+    805,
+    "ABS",
+    ["DF1F26", "#141414", "#FFFFFF"],
+    "Fresh vibes. Sporty soul."
+  ),
+];
+
+export const languages = [
+  {
+    Displacement: "Displacement",
+    Horse_Power: "Horse Power",
+    Torque: "Torque",
+    Dry_Weight: "Dry Weight",
+    set_height: "Seat Height",
+    safety: "safety",
+  },
+  {
+    Displacement: "Kubatūra",
+    Horse_Power: "Zirgspēki",
+    Torque: "Griezes moments",
+    Dry_Weight: "Pamat svars",
+    set_heigh: "Sēd pozīcijas augstumns",
+    safety: "Drošiba",
+  },
+];
--- moto_page/src/motoSelect/motoselect.js
+++ moto_page/src/motoSelect/motoselect.js
@@ -1,15 +1,37 @@
-import React from 'react';
-import "./motoselect.scss"
-
+import React from "react";
+import "./motoselect.scss";
+import { motorSpec, languages } from "../data/data";
 
 const MotoSelect = () => {
-    return ( 
+  return (
     <main>
-        <div className="brandLogo"></div>
-        <div className="motoDisplay"></div>
-        <div className="details"></div>
-    </main> 
-    );
-}
- 
+      <div className="brandLogo"></div>
+      <div className="motoDisplay"></div>
+      <div className="details">
+        <div className="generalInfo">
+          <p>{languages[0].Displacement}</p>
+          <p>{motorSpec[0].cc}</p>
+
+          <p>{languages[0].Horse_Power}</p>
+          <p>{motorSpec[0].hp}</p>
+
+          <p>{languages[0].Torque}</p>
+          <p>{motorSpec[0].nm}</p>
+
+          <p>{languages[0].Dry_Weight}</p>
+          <p>{motorSpec[0].weight}</p>
+
+          <p>{languages[0].set_heigh}</p>
+          <p>{motorSpec[0].seatHeight}</p>
+
+          <p>{languages[0].safety}</p>
+          <p>{motorSpec[0].brake}</p>
+        </div>
+        <div className="colorAvailable"></div>
+        <p className="Detail__desc">{motorSpec[0].desc}</p>
+      </div>
+    </main>
+  );
+};
+
 export default MotoSelect;
